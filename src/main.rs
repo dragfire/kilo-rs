267e0697d960a9df388041447cd7f3d36a4f17be
dragfire <asem.devajit@gmail.com>
2020-06-09 21:13:33 -0400

    Make terminal work with STDIN

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,48 @@
 use std::io::{self, Read};
 use std::os::unix::io::{AsRawFd, RawFd};
-use std::process::exit;
 use termios::*;
 
-fn enable_raw_mode(fd: RawFd) -> Result<(), io::Error> {
+fn enable_raw_mode(fd: RawFd) -> Result<Termios, io::Error> {
     let mut raw: Termios = Termios::from_fd(fd)?;
+    let orig_raw = raw;
     tcgetattr(fd, &mut raw)?;
-    raw.c_lflag &= !(ECHO);
+    raw.c_iflag &= !(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
+    raw.c_oflag &= !(OPOST);
+    raw.c_cflag |= CS8;
+    raw.c_lflag &= !(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
     tcsetattr(fd, TCSAFLUSH, &mut raw)?;
+    Ok(orig_raw)
+}
+
+fn disable_raw_mode(raw: Termios) -> Result<(), io::Error> {
+    let mut raw = raw;
+    tcsetattr(io::stdin().as_raw_fd(), TCSAFLUSH, &mut raw)?;
     Ok(())
 }
 
 fn main() {
     let mut stdin = io::stdin();
-    enable_raw_mode(stdin.as_raw_fd()).unwrap();
-    while let Some(b) = stdin.by_ref().bytes().next() {
-        let c = b.ok().unwrap() as char;
+    let orig_raw = enable_raw_mode(stdin.as_raw_fd()).unwrap();
+
+    loop {
+        let byte = stdin.by_ref().bytes().next();
+        let c = match byte {
+            Some(ch) => ch.ok().unwrap() as char,
+            None => '\0',
+        };
+
+        if c.is_ascii_control() {
+            println!("{}\r", c as u8);
+        } else {
+            println!("{},({})\r", c as u8, c);
+        }
+
         if c == 'q' {
-            exit(0);
+            break;
         }
     }
+
+    disable_raw_mode(orig_raw).unwrap();
 }
