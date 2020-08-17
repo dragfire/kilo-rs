8244a4e845f25edcdfcfa4559417f3a2e009c6ee
dragfire <asem.devajit@gmail.com>
2020-08-16 18:25:17 -0600

    Highlight multiline comments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 use std::env;
 use std::fs::{File, OpenOptions};
-use std::io::{self, BufRead, BufReader, Read, Write};
+use std::io::{self, Read, Write};
 use std::iter::FromIterator;
 use std::os::unix::io::{AsRawFd, RawFd};
 use std::path::Path;
@@ -19,9 +19,11 @@ const KILO_QUIT_TIMES: usize = 3;
 /// Supports rendering tabs or spaces and syntax highlighting.
 #[derive(Default)]
 struct Row {
+    idx: usize,
     chars: String,
     render: String,
     hl: Vec<Highlight>,
+    hl_open_comment: bool,
 }
 
 #[derive(Eq, PartialEq)]
@@ -34,6 +36,7 @@ enum Direction {
 enum Highlight {
     Normal,
     Comment,
+    MLComment,
     Keyword1,
     Keyword2,
     String,
@@ -44,7 +47,7 @@ enum Highlight {
 impl From<Highlight> for i32 {
     fn from(hl: Highlight) -> i32 {
         match hl {
-            Highlight::Comment => 36,
+            Highlight::Comment | Highlight::MLComment => 36,
             Highlight::Keyword1 => 33,
             Highlight::Keyword2 => 32,
             Highlight::String => 35,
@@ -131,6 +134,8 @@ impl EditorConfig {
             HashSet::from_iter(c_filematch),
             c_keywords,
             "//".to_string(),
+            "/*".to_string(),
+            "*/".to_string(),
             HighlightFlag::Number as u8 | HighlightFlag::String as u8,
         ));
 
@@ -186,6 +191,8 @@ struct EditorSyntax {
     filematch: HashSet<String>,
     keywords: Vec<String>,
     singleline_comment_start: String,
+    multiline_comment_start: String,
+    multiline_comment_end: String,
     flags: u8,
 }
 
@@ -195,6 +202,8 @@ impl EditorSyntax {
         filematch: HashSet<String>,
         keywords: Vec<String>,
         singleline_comment_start: String,
+        multiline_comment_start: String,
+        multiline_comment_end: String,
         flags: u8,
     ) -> Self {
         EditorSyntax {
@@ -202,6 +211,8 @@ impl EditorSyntax {
             filematch,
             keywords,
             singleline_comment_start,
+            multiline_comment_start,
+            multiline_comment_end,
             flags,
         }
     }
@@ -284,96 +295,159 @@ fn disable_raw_mode(raw: &Termios) -> Result<(), io::Error> {
 
 // *** Syntax Highlighting ***
 
-fn editor_update_syntax(syntax: Option<&EditorSyntax>, row: &mut Row) {
+fn editor_update_syntax(edit_syntax: Option<&EditorSyntax>, rows: &mut [Row], cy: usize) {
+    let rows = std::cell::RefCell::new(rows);
+    let numrows = rows.borrow().len();
+    let mut brows = rows.borrow_mut();
+    let (left, right) = brows.split_at_mut(cy);
+    let row = &mut right[0];
+    let prev_row = left.last();
+    row.hl = vec![Highlight::Normal; row.chars.len()];
+
+    if let Some(syntax) = edit_syntax {
+        let mut in_comment = row.idx > 0 && prev_row.map(|r| r.hl_open_comment).unwrap_or(false);
+        apply_syntax(syntax, in_comment, row);
+        let changed = row.hl_open_comment != in_comment;
+        row.hl_open_comment = in_comment;
+        while changed && row.idx + 1 < numrows {
+            let mut brows = rows.borrow_mut();
+            let (left, right) = brows.split_at_mut(row.idx + 1);
+            let row = &mut right[0];
+            let prev_row = left.last();
+            in_comment = row.idx > 0 && prev_row.map(|r| r.hl_open_comment).unwrap_or(false);
+            apply_syntax(syntax, in_comment, row);
+        }
+    }
+}
+
+fn apply_syntax(syntax: &EditorSyntax, mut in_comment: bool, row: &mut Row) {
     let n = row.render.len();
-    row.hl = vec![Highlight::Normal; n];
+    let mut prev_sep = true;
+    let mut in_string = false;
+    let flags = syntax.flags;
 
-    if let Some(syntax) = syntax {
-        let mut prev_sep = true;
-        let mut in_string = false;
-        let flags = syntax.flags;
-        let scs = &syntax.singleline_comment_start;
-        let scs_len = scs.len();
-        let keywords = &syntax.keywords;
+    let scs = &syntax.singleline_comment_start;
+    let mcs = &syntax.multiline_comment_start;
+    let mce = &syntax.multiline_comment_end;
 
-        for (i, c) in row.render.chars().enumerate() {
-            let prev_hl = if i > 0 {
-                row.hl[i - 1]
-            } else {
-                Highlight::Normal
-            };
+    let scs_len = scs.len();
+    let mcs_len = mcs.len();
+    let mce_len = mce.len();
+    let keywords = &syntax.keywords;
 
-            if scs_len > 0 && !in_string {
-                let slice = &row.render[i..];
-                if slice.starts_with(scs) {
-                    let slice = &mut row.hl[i..];
-                    for el in slice {
-                        *el = Highlight::Comment;
-                    }
-                    break;
+    let row_render_slice = row.render.as_bytes();
+    let mut i = 0;
+    while i < n {
+        let slice = &row_render_slice[i..];
+        let c = row_render_slice[i] as char;
+        let prev_hl = if i > 0 {
+            row.hl[i - 1]
+        } else {
+            Highlight::Normal
+        };
+
+        if scs_len > 0 && !in_string && !in_comment {
+            if slice.starts_with(scs.as_bytes()) {
+                let slice = &mut row.hl[i..];
+                for el in slice {
+                    *el = Highlight::Comment;
                 }
+                break;
             }
+        }
 
-            if syntax.flags & HighlightFlag::String as u8 == HighlightFlag::String as u8 {
-                if in_string {
-                    row.hl[i] = Highlight::String;
-                    if c == '\\' && i + 1 < n {
-                        row.hl[i + 1] = Highlight::String;
-                        continue;
+        if mcs_len > 0 && mce_len > 0 && !in_string {
+            if in_comment {
+                row.hl[i] = Highlight::MLComment;
+                if row.render.starts_with(mce) {
+                    let slice = &mut row.hl[i..i + mce_len];
+                    for el in slice {
+                        *el = Highlight::MLComment;
                     }
+                    i += mce_len;
+                    in_comment = false;
                     prev_sep = true;
                     continue;
                 } else {
-                    if c == '"' || c == '\'' {
-                        in_string = true;
-                        row.hl[i] = Highlight::String;
-                        continue;
-                    }
+                    i += 1;
+                    continue;
                 }
+            } else if slice.starts_with(mcs.as_bytes()) {
+                let slice = &mut row.hl[i..i + mcs_len];
+                for el in slice {
+                    *el = Highlight::MLComment;
+                }
+                i += mcs_len;
+                in_comment = true;
+                continue;
             }
+        }
 
-            if flags & HighlightFlag::Number as u8 == HighlightFlag::Number as u8 {
-                if c.is_ascii_digit() && (prev_sep || prev_hl == Highlight::Number)
-                    || (c == '.' && prev_hl == Highlight::Number)
-                {
-                    row.hl[i] = Highlight::Number;
-                    prev_sep = false;
+        if syntax.flags & HighlightFlag::String as u8 == HighlightFlag::String as u8 {
+            if in_string {
+                row.hl[i] = Highlight::String;
+                if c == '\\' && i + 1 < n {
+                    row.hl[i + 1] = Highlight::String;
+                    i += 2;
+                    continue;
+                }
+                i += 1;
+                prev_sep = true;
+                continue;
+            } else {
+                if c == '"' || c == '\'' {
+                    in_string = true;
+                    row.hl[i] = Highlight::String;
+                    i += 1;
                     continue;
                 }
             }
+        }
 
-            if prev_sep {
-                for keyword in keywords.iter() {
-                    let mut kw = keyword.as_str();
-                    let mut klen = keyword.len();
-                    let kw2 = keyword.get(klen - 1..);
-                    let mut is_kw2 = false;
-                    if let Some(kw2) = kw2 {
-                        if kw2 == "|" {
-                            klen -= 1;
-                            kw = keyword.get(..klen - 1).unwrap();
-                            is_kw2 = true;
-                        }
-                    }
+        if flags & HighlightFlag::Number as u8 == HighlightFlag::Number as u8 {
+            if c.is_ascii_digit() && (prev_sep || prev_hl == Highlight::Number)
+                || (c == '.' && prev_hl == Highlight::Number)
+            {
+                row.hl[i] = Highlight::Number;
+                i += 1;
+                prev_sep = false;
+                continue;
+            }
+        }
 
-                    let slice = &row.render[i..];
-                    let bytes = slice.as_bytes();
-                    if slice.starts_with(kw) && is_seperator(bytes[klen] as char) {
-                        let slice = &mut row.hl[i..i + klen];
-                        for el in slice {
-                            *el = if is_kw2 {
-                                Highlight::Keyword2
-                            } else {
-                                Highlight::Keyword1
-                            };
-                        }
-                        break;
+        if prev_sep {
+            for keyword in keywords.iter() {
+                let mut kw = keyword.as_str();
+                let mut klen = keyword.len();
+                let kw2 = keyword.get(klen - 1..);
+                let mut is_kw2 = false;
+                if let Some(kw2) = kw2 {
+                    if kw2 == "|" {
+                        klen -= 1;
+                        kw = keyword.get(..klen - 1).unwrap();
+                        is_kw2 = true;
                     }
                 }
-            }
 
-            prev_sep = is_seperator(c);
+                let slice = &row.render[i..];
+                let bytes = slice.as_bytes();
+                if slice.starts_with(kw) && is_seperator(bytes[klen] as char) {
+                    let slice = &mut row.hl[i..i + klen];
+                    for el in slice {
+                        *el = if is_kw2 {
+                            Highlight::Keyword2
+                        } else {
+                            Highlight::Keyword1
+                        };
+                    }
+                    i += klen;
+                    break;
+                }
+            }
         }
+
+        prev_sep = is_seperator(c);
+        i += 1;
     }
 }
 
@@ -393,8 +467,12 @@ fn editor_select_syntax_highlight(cfg: &mut EditorConfig) {
         for syntax in cfg.hldb.iter() {
             if syntax.filematch.contains(ext.to_str().unwrap()) {
                 cfg.editor_syntax = Some(syntax.clone());
-                for row in cfg.rows.iter_mut() {
-                    editor_update_syntax(cfg.editor_syntax.as_ref(), row);
+                for _ in 0..cfg.numrows {
+                    editor_update_syntax(
+                        cfg.editor_syntax.as_ref(),
+                        cfg.rows.as_mut_slice(),
+                        cfg.cy,
+                    );
                 }
                 return;
             }
@@ -439,16 +517,24 @@ fn editor_insert_row(cfg: &mut EditorConfig, chars: String, at: usize) {
     if at > cfg.numrows {
         return;
     }
+
+    for j in at + 1..cfg.numrows + 1 {
+        cfg.rows[j].idx += 1;
+    }
+
     let mut row = Row::default();
+    row.idx = at;
     row.chars = chars;
-    editor_update_row(cfg.editor_syntax.as_ref(), &mut row);
+    row.hl_open_comment = false;
     cfg.rows.insert(at, row);
     cfg.numrows = cfg.rows.len();
     cfg.dirty = true;
+    editor_update_row(cfg.editor_syntax.as_ref(), cfg.rows.as_mut_slice(), at);
 }
 
-fn editor_update_row(syntax: Option<&EditorSyntax>, row: &mut Row) {
+fn editor_update_row(syntax: Option<&EditorSyntax>, rows: &mut [Row], cy: usize) {
     let mut idx = 0;
+    let row = &mut rows[cy];
     row.render.clear();
 
     for c in row.chars.chars() {
@@ -465,7 +551,7 @@ fn editor_update_row(syntax: Option<&EditorSyntax>, row: &mut Row) {
         }
     }
 
-    editor_update_syntax(syntax, row);
+    editor_update_syntax(syntax, rows, cy);
 }
 
 fn editor_free_row(row: &mut Row) {
@@ -479,29 +565,41 @@ fn editor_del_row(cfg: &mut EditorConfig, at: usize) {
         return;
     }
     editor_free_row(&mut cfg.rows[cfg.cy]);
+    for j in at..cfg.numrows - 1 {
+        cfg.rows[j].idx -= 1;
+    }
     cfg.rows.remove(at);
     cfg.dirty = true;
 }
 
-fn editor_row_insert_char(syntax: Option<&EditorSyntax>, row: &mut Row, mut at: usize, c: char) {
+fn editor_row_insert_char(
+    syntax: Option<&EditorSyntax>,
+    rows: &mut [Row],
+    mut at: usize,
+    c: char,
+    cy: usize,
+) {
+    let row = &mut rows[cy];
     if at > row.chars.len() {
         at = row.chars.len();
     }
     row.chars.insert(at, c);
-    editor_update_row(syntax, row);
+    editor_update_row(syntax, rows, at);
 }
 
-fn editor_row_del_char(syntax: Option<&EditorSyntax>, row: &mut Row, at: usize) {
+fn editor_row_del_char(syntax: Option<&EditorSyntax>, rows: &mut [Row], at: usize, cy: usize) {
+    let row = &mut rows[cy];
     if at >= row.chars.len() {
         return;
     }
     row.chars.remove(at);
-    editor_update_row(syntax, row);
+    editor_update_row(syntax, rows, cy);
 }
 
-fn editor_row_append_str(syntax: Option<&EditorSyntax>, row: &mut Row, s: &str) {
+fn editor_row_append_str(syntax: Option<&EditorSyntax>, rows: &mut [Row], cy: usize, s: &str) {
+    let row = &mut rows[cy];
     row.chars.push_str(s);
-    editor_update_row(syntax, row);
+    editor_update_row(syntax, rows, cy);
 }
 
 // *** Editor operations ***
@@ -510,7 +608,14 @@ fn editor_insert_char(cfg: &mut EditorConfig, c: char) {
     if cfg.cy == cfg.numrows {
         editor_insert_row(cfg, String::new(), 0);
     }
-    editor_row_insert_char(cfg.editor_syntax.as_ref(), &mut cfg.rows[cfg.cy], cfg.cx, c);
+    editor_row_insert_char(
+        cfg.editor_syntax.as_ref(),
+        cfg.rows.as_mut_slice(),
+        cfg.cx,
+        c,
+        cfg.cy,
+    );
+
     cfg.cx += 1;
     cfg.dirty = true;
 }
@@ -522,9 +627,9 @@ fn editor_insert_new_line(cfg: &mut EditorConfig) {
         let chars = cfg.rows[cfg.cy].chars.to_owned();
         editor_insert_row(cfg, String::from(&chars[cfg.cx..]), cfg.cy + 1);
 
-        let mut row = &mut cfg.rows[cfg.cy];
+        let row = &mut cfg.rows[cfg.cy];
         row.chars = String::from(&chars[..cfg.cx]);
-        editor_update_row(cfg.editor_syntax.as_ref(), &mut row);
+        editor_update_row(cfg.editor_syntax.as_ref(), cfg.rows.as_mut_slice(), cfg.cy);
     }
     cfg.cy += 1;
     cfg.cx = 0;
@@ -539,15 +644,22 @@ fn editor_del_char(cfg: &mut EditorConfig) {
     }
 
     if cfg.cy <= cfg.rows.len() {
-        let (left, right) = cfg.rows.split_at_mut(cfg.cy);
-        let row = &mut right[0];
         if cfg.cx > 0 {
-            editor_row_del_char(cfg.editor_syntax.as_ref(), row, cfg.cx - 1);
+            editor_row_del_char(
+                cfg.editor_syntax.as_ref(),
+                cfg.rows.as_mut_slice(),
+                cfg.cx - 1,
+                cfg.cy,
+            );
             cfg.cx -= 1;
         } else {
-            let prev_row = &mut left[left.len() - 1];
-            cfg.cx = prev_row.chars.len();
-            editor_row_append_str(cfg.editor_syntax.as_ref(), prev_row, row.chars.as_str());
+            let chars = &cfg.rows[cfg.cy].chars.to_string();
+            editor_row_append_str(
+                cfg.editor_syntax.as_ref(),
+                cfg.rows.as_mut_slice(),
+                cfg.cy,
+                chars,
+            );
             editor_del_row(cfg, cfg.cy);
             cfg.cy -= 1;
         }
@@ -1003,8 +1115,8 @@ fn editor_process_keypress(cfg: &mut EditorConfig) {
                     editor_set_status_msg(
                         cfg,
                         format!(
-                            "WARNING!!! File has unsaved changes. \
-                            Press Ctrl-Q {} more times to quit.",
+                            "\x1b[31mWARNING!!! File has unsaved changes. \
+                            Press Ctrl-Q {} more times to quit.\x1b[39m",
                             cfg.quit_times,
                         ),
                     );
@@ -1087,7 +1199,6 @@ fn editor_open(cfg: &mut EditorConfig, filename: &str) {
             for (i, line) in lossy_content.lines().enumerate() {
                 editor_insert_row(cfg, line.to_string(), i);
             }
-            cfg.numrows = cfg.rows.len();
             cfg.dirty = false;
         }
         Err(e) => {
